152e67145f4541cd8806840386f0ba1fdba579e9
Neveen-E <nelasar@spartaglobal.com>
2018-07-27 09:46:06 +0100

    kitchem.yml done

--- spec/unit/recipes/mongodb_spec.rb
+++ spec/unit/recipes/mongodb_spec.rb
@@ -1,16 +1,27 @@
 require 'spec_helper'
 
-describe 'mongodb::default' do
+describe 'mongodb::mongodb' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+
       runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'runs apt-get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+    it 'should install nginx' do
+      expect(chef_run).to install_package 'nginx'
+    end
+
+    it 'should start nginx service' do
+      expect
     end
   end
 end
