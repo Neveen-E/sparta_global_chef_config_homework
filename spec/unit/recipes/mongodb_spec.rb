2b800497b119147b872b5cda6c9eb49015f20ef6
Neveen-E <nelasar@spartaglobal.com>
2018-07-27 15:37:03 +0100

    all tests pass

--- spec/unit/recipes/mongodb_spec.rb
+++ spec/unit/recipes/mongodb_spec.rb
@@ -7,21 +7,37 @@ describe 'mongodb::mongodb' do
       runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'runs apt get update' do
+      expect(chef_run).to update_apt_update 'update'
     end
 
-    it 'runs apt-get update' do
-      expect(chef_run).to update_apt_update 'update_sources'
+    it 'should add mongodb to the source list' do
+      expect(chef_run).to add_apt_repository 'mongodb-org'
     end
 
-    it 'should install nginx' do
-      expect(chef_run).to install_package 'nginx'
+    it 'should install mongodb and upgrade' do
+      expect(chef_run).to upgrade_package 'mongodb-org'
     end
 
-    it 'should start nginx service' do
-      expect
+    it 'should start service mongod'do
+      expect(chef_run).to start_service 'mongod'
     end
+
+    it 'should enable service mongod'do
+      expect(chef_run).to enable_service 'mongod'
+    end
+
+    it 'should update the mongo service config' do
+      expect(chef_run).to create_template('/lib/systemd/system/mongod.service').with(source: 'mongod.service.erb')
+      template = chef_run.template('/lib/systemd/system/mongod.service')
+      expect(template).to notify('service[mongod]')
+    end
+
+    it 'should update the mongod config' do
+      expect(chef_run).to create_template('/etc/mongod.conf').with_variables(port: 27017, ip_addresses: ['0.0.0.0'])
+      template = chef_run.template('/etc/mongod.conf')
+      expect(template).to notify('service[mongod]')
+    end
+      at_exit { ChefSpec::Coverage.report! }
   end
 end
